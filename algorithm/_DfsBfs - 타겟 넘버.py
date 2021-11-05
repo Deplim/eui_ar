c2ee688c40543d3f86670e6de2e82e6c17e781b8
Eui Ryeong <37531737+Deplim@users.noreply.github.com>
2021-11-06 04:48:26 +0900

    Update _DfsBfs - 타겟 넘버.py
    메인 함수와 서브 함수 순서 바꿈

--- algorithm/_DfsBfs - 타겟 넘버.py
+++ algorithm/_DfsBfs - 타겟 넘버.py
@@ -3,6 +3,13 @@
 
 # solution2 -> solution : 재귀함수 실행시 배열이 아니라 index 를 넘기도록 구현
 
+def solution(numbers, target):
+    global numbers_, numbers_len
+    numbers_ = numbers[:]
+    numbers_len = len(numbers)
+
+    return full_search(0, target)
+
 def full_search(number, target):
     plus = target - numbers_[number]
     minus = target + numbers_[number]
@@ -11,22 +18,14 @@ def full_search(number, target):
         return (plus == 0) or (minus == 0)
     else:
         return full_search(number+1, plus)+full_search(number+1, minus)
+        
 
-def solution(numbers, target):
-    global numbers_, numbers_len
-    numbers_ = numbers[:]
-    numbers_len = len(numbers)
-
-    return full_search(0, target)
-
-
+def solution2(numbers, target):
+    answer = full_search2(numbers, target)
+    return answer
 
 def full_search2(numbers,target):
     if len(numbers)==1:
         return (target-numbers[0]==0) or (target+numbers[0]==0)
     else:
         return full_search2(numbers[1:], target-numbers[0])+full_search2(numbers[1:], target+numbers[0])
-
-def solution2(numbers, target):
-    answer = full_search2(numbers, target)
-    return answer
