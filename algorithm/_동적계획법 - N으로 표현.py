c88b1b2f9747428571411415dda7f209a8e453d7
Eui Ryeong <37531737+Deplim@users.noreply.github.com>
2021-11-05 11:14:02 +0900

    Create _동적계획법 - N으로 표현.py

---
+++ algorithm/_동적계획법 - N으로 표현.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+def solution(N, number):
+    storage = {}
+    storage[1] = [N]
+    if number == N: 
+        return 1
+    
+    for i in range(2, 9):
+        cont = int(str(N)*i)   
+
+        cases = []
+        for j in range(1, i):
+            temp = np.array([[[a+b, a-b, a*b, a//b if b!=0 else None] for b in storage[i-j]] for a in storage[j]])
+            cases += temp.reshape(-1,).tolist()
+        cases = list(set(cases)) + [cont]
+        
+        if None in cases:
+            cases.remove(None)
+
+        if number in cases:
+            return i
+
+        storage[i] = cases
+        if number in cases:
+            return i
+    return -1
