67fb617da47c9d49215f109f1cacc9a48d57a37b
Eui Ryeong <37531737+Deplim@users.noreply.github.com>
2021-11-16 14:56:34 +0900

    Update _graph - 가장 먼 노드.py
    다른 유형의 풀이 추가함

--- algorithm/_graph - 가장 먼 노드.py
+++ algorithm/_graph - 가장 먼 노드.py
@@ -2,6 +2,7 @@
 # EuiRyeong Jeong (wjddmflud@gmail.com, https://github.com/Deplim)
 
 # solution2 -> solution : list 대신 deque 사용, visit 를 set 으로 구현
+# visit 처리를 queue 에서 값을 꺼낼 때 할지, 연결된 노드를 찾을 때 할지에 따라 구현이 달라지는데, 어느쪽으로 풀어도 됨.
 
 from collections import deque
 
@@ -28,6 +29,27 @@ def solution(n, edge):
     answer = answer.count(max(answer))
     return answer
 
+def solution_(n, edge):
+    answer = [-1]*(n+1)
+    G = {i+1:[] for i in range(n)}
+    for a,b in edge:
+        G[a].append(b)
+        G[b].append(a)
+    
+    visit = set([])
+    queue = deque([(1, 0)])
+
+    while queue:
+        node, path=queue.popleft()
+        if node not in visit:  
+            visit.add(node)
+            answer[node]=path
+
+            temp=[(i, path+1) for i in G[node]]
+            queue += temp
+            
+    answer = answer.count(max(answer))
+    return answer
 
 '''
 def solution2(n, edge):
