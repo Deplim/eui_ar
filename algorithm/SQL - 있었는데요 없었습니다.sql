45ed45f2c76d148ffc02e2bfee3f5fbadfb3379b
Eui Ryeong <37531737+Deplim@users.noreply.github.com>
2021-11-20 11:49:17 +0900

    Create SQL - 있었는데요 없었습니다.sql

---
+++ algorithm/SQL - 있었는데요 없었습니다.sql
@@ -0,0 +1,7 @@
+#프로그래머스 | 우유와 요거트 : (https://programmers.co.kr/learn/courses/30/lessons/59043)
+# EuiRyeong Jeong (wjddmflud@gmail.com, https://github.com/Deplim)
+
+SELECT I.ANIMAL_ID, O.NAME
+FROM ANIMAL_INS AS I, ANIMAL_OUTS AS O
+WHERE I.ANIMAL_ID=O.ANIMAL_ID AND I.DATETIME > O.DATETIME
+ORDER BY I.DATETIME
