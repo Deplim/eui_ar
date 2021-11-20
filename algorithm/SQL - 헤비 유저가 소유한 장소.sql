c35a8da0ddb5191d45b8d52d30c65180bd72f00e
Eui Ryeong <37531737+Deplim@users.noreply.github.com>
2021-11-20 11:51:24 +0900

    create SQL - 헤비 유저가 소유한 장소.sql

---
+++ algorithm/SQL - 헤비 유저가 소유한 장소.sql
@@ -0,0 +1,7 @@
+#프로그래머스 | 입양 시각 구하기(2) : (https://programmers.co.kr/learn/courses/30/lessons/59413)
+# EuiRyeong Jeong (wjddmflud@gmail.com, https://github.com/Deplim)
+
+SELECT *
+from PLACES
+WHERE HOST_ID in (SELECT HOST_ID FROM PLACES GROUP BY HOST_ID HAVING count(*)>=2)
+ORDER BY ID
