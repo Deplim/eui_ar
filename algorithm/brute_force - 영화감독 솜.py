7e123d78641a41e8de1663a8e0847d66f6f2da95
Eui Ryeong <37531737+Deplim@users.noreply.github.com>
2022-03-19 01:53:33 +0900

    brute_force - 영화감독 솜.py

--- algorithm/brute_force - 영화감독 솜.py
+++ algorithm/brute_force - 영화감독 솜.py
@@ -1,2 +1,17 @@
 # 백준 | 영화감독 솜 : (https://www.acmicpc.net/problem/1436)
 # EuiRyeong Jeong (wjddmflud@gmail.com, https://github.com/Deplim)
+
+def solution(input):
+    count = 0
+    num = 666
+    while 1:
+        if '666' in str(num):
+            count += 1
+            if count == input:
+                return num
+        num += 1
+
+
+input = int(input())
+Answer = solution(input)
+print(Answer)
