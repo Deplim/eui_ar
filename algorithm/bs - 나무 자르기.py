ed7f4187aed379cb79d5d01d8f2d7b2f0d3fe44c
Eui Ryeong <37531737+Deplim@users.noreply.github.com>
2022-03-20 00:10:01 +0900

    bs - 나무 자르기.py 주석 추가

--- algorithm/bs - 나무 자르기.py
+++ algorithm/bs - 나무 자르기.py
@@ -7,15 +7,15 @@ import math
 def solution(n, m, input_):
     input_.sort()
     total_sum = sum(input_)
-    c_sum = 0
+    c_sum = 0  # 지금 차례까지의 나무 길이의 합 (c_sum)
     end = 0
     for i, v in enumerate(input_):
         c_sum += v
-        tree_under = c_sum + (n - (i+1)) * v
+        tree_under = c_sum + (n - (i+1)) * v  # tree_under 는 자르고 남는 나무의 길이
         if (total_sum - tree_under) < m:
             end = v
             break
-    target = m-(total_sum - tree_under)
+    target = m-(total_sum - tree_under)  # end 에서 자를경우 부족한 나무의 길이
     Answer = end - math.ceil(target / (n-i))
     return Answer
 
